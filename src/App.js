78a806aa2048de40f30f680422f8bec257a69f02
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2022-05-11 07:46:50 +0100

    Set greeting and deposit eth to contract

--- src/App.js
+++ src/App.js
@@ -4,21 +4,22 @@ import ABI from './ABI.json';
 
 function App() {
     const [balance, setBalance] = useState();
-    const [deposit, setDeopsit] = useState();
+    const [deposit, setDeposit] = useState(0);
     const [greeting, setGreeting] = useState();
+    const [greetingValue, setGreetingValue] = useState('');
     const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
     const provider = new ethers.providers.Web3Provider(window.ethereum);
     const signer = provider.getSigner()
     const contract = new ethers.Contract(contractAddress, ABI, signer)
 
+    const getBalance = async () => {
+        const balance = await provider.getBalance(contractAddress)
+        setBalance(ethers.utils.formatEther(balance))
+    }
 
     useEffect(() => {
         const connectWallet = async () => {
             await provider.send("eth_requestAccounts", [])
-        }
-        const getBalance = async () => {
-            const balance = await provider.getBalance(contractAddress)
-            setBalance(ethers.utils.formatEther(balance))
         }
 
         const getGreeting = async () => {
@@ -34,13 +35,20 @@ function App() {
             .catch(console.error)
     }, [])
 
-    const handleDepositSubmit = (e) => {
+    const handleDepositSubmit = async (e) => {
         e.preventDefault();
+        const value = ethers.utils.parseEther(deposit)
+        const transaction = await contract.deposit({value})
+        await transaction.wait()
+        await getBalance()
     }
 
-    const handleGreetingSubmit = (e) => {
+    const handleGreetingSubmit = async (e) => {
         e.preventDefault();
-
+        const greeting = await contract.setGreeting(greetingValue);
+        await greeting.wait()
+        setGreeting(greetingValue)
+        setGreetingValue('')
     }
 
     return (
@@ -54,15 +62,15 @@ function App() {
                     <form action="" onSubmit={handleDepositSubmit}>
                         <div className="mb-3">
                             <input type="number" className={"form-control"} placeholder={0} value={deposit}
-                                   onChange={e => setDeopsit(e.target.value)}/>
+                                   onChange={e => setDeposit(e.target.value)}/>
                         </div>
                         <button type={"submit"} className={"btn btn-success"}>Deposit</button>
                     </form>
 
                     <form action="" className={"mt-3"} onSubmit={handleGreetingSubmit}>
                         <div className="mb-3">
-                            <input type="text" className={"form-control"} placeholder={"Greeting"} value={greeting}
-                                   onChange={e => setGreeting(e.target.value)}/>
+                            <input type="text" className={"form-control"} placeholder={"Greeting"} value={greetingValue}
+                                   onChange={e => setGreetingValue(e.target.value)}/>
                         </div>
                         <button type={"submit"} className={"btn btn-dark"}>Change</button>
                     </form>
